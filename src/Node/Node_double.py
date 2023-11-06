78e6a365e988cb3847080225a3033a91d19e02df
Hans Magne Asheim <hmasheim@stud.ntnu.no>
2023-11-06 16:11:04 +0100

    refactor node: rewards in node

--- src/Node/Node_double.py
+++ src/Node/Node_double.py
@@ -1,83 +1,79 @@
 import numpy as np
-from Connect_four_env import ConnectFour
 
-
-class NodeDouble():
-    
-    def __init__(self, parent=None, env=ConnectFour(), action=None):
+class NodeDouble:
+    def __init__(self, env, state, parent=None, action_taken=None):
+        if not env.check_state_format(state):
+            print("ERROR: In state format Node constructor")
+        
         self.children = []
         self.parent = parent
-        self.action = action
+        self.action_taken = action_taken
         self.env = env
         self.reward1 = 0 # Player 1 reward
         self.reward2 = 0 # Player 2 reward
         self.visits = 0
+        self.state = state
         
-        self.c = 4 # Exploration parameter
+        self.c = 4  # Exploration parameter
         
-    def calculate_UCB(self, child):
-        if child.visits == 0:
-            return float('inf')
+        self.expandable_moves = env.get_legal_moves_bool_array(state)
         
-        return self.env.get_player() * np.abs(child.reward1 - child.reward2) / child.visits + np.sqrt(self.c * np.log(self.visits) / child.visits)
-
-    # def select(self):
-    #     node = self
-    #     while bool(node.children): 
-    #         UCB_values = []
-    #         for child in node.children:
-    #             UCB_values.append(node.calculate_UCB(child))
-    #         node = node.children[np.argmax(UCB_values)]
-    
-    #     return node
+    def is_fully_expanded(self):
+        return np.sum(self.expandable_moves) == 0 and len(self.children) > 0
     
     def select(self):
         node = self
         best_child = self
-        while bool(node.children):
+        while node.is_fully_expanded():
             max_UCB = float('-inf')
             for child in node.children:
-                UCB_value = node.calculate_UCB(child)
+                UCB_value = node.get_ucb(child)
                 if UCB_value > max_UCB:
                     max_UCB = UCB_value
                     best_child = child
             node = best_child
         return best_child
+    
+    def get_ucb(self, child): # TODO: rewrite
+        if child.visits == 0:
+            return float('inf')
 
-    # Adds a child node for untried action and returns it
+        return -np.abs(child.reward1 - child.reward2) / child.visits + np.sqrt(self.c * np.log(self.visits) / child.visits)
+        
     def expand(self):
         if self.visits > 0:
-            legal_moves = self.env.get_legal_moves()
+            legal_moves = self.env.get_legal_moves(self.state)
             for action in legal_moves:
-                next_env = self.env.deepcopy()
-                next_env.step(action)
-                child = NodeDouble(parent=self, env=next_env, action=action)
+                self.expandable_moves[action] = 0
+                child_state = self.state.copy()
+                child_state, reward, done = self.env.step(child_state, action, 1)
+                child_state = child_state = self.env.change_perspective(child_state, player=-1)
                 
+                child = NodeDouble(self.env, child_state, self, action)
                 self.children.append(child)
-            
-    def random_act(self, env):
-        return np.random.choice(env.get_legal_moves())
-
-    # Simulate random actions until a terminal state is reached
-    def simulate(self):
-        simulated_game = self.env.deepcopy()
-        current_player = simulated_game.get_last_player() # last player because step is called in expand
-        reward, done = simulated_game.check_game_over(current_player)
-            
-        while not done:
-            action = self.random_act(simulated_game)
-            reward, done = simulated_game.step(action)
-
-        winning_player = simulated_game.get_last_player() # last player becuase the last player won
-        
-        # print("Board:\n")
-        # print(simulated_game.board)
-        # if simulated_game.is_inverted:
-        #     print("\n",-simulated_game.board)
-            
-        
-        return reward * winning_player, simulated_game.board # Return 1 if the player won, -1 if the player lost, and 0 if it was a draw.
     
+    def random_action(self, env, state):
+        return np.random.choice(env.get_legal_moves(state))
+    
+    def simulate(self):
+        reward, done = self.env.check_game_over(self.state, self.action_taken)
+        reward = self.env.get_opponent_value(reward)
+        
+        if done:
+            return reward
+        
+        rollout_state = self.state.copy()
+        rollout_player = 1
+        
+        while not done:
+            rollout_action = self.random_action(self.env, rollout_state)
+            rollout_state, reward, done = self.env.step(rollout_state, rollout_action, rollout_player)
+            rollout_player = self.env.get_opponent(rollout_player)
+            
+        if self.env.get_opponent(rollout_player) == -1:
+            reward = self.env.get_opponent_value(reward)
+        return reward, rollout_state 
+            
     def backpropagate(self, result):
         self.visits += 1
         if result == 1:
@@ -86,4 +82,7 @@ class NodeDouble():
             self.reward2 += 1
         
         if self.parent:
+            # TODO: might get oppoenent value for result?
             self.parent.backpropagate(result)
+
+        
--- src/Node/Node_single.py
+++ src/Node/Node_single.py
@@ -1,66 +1,84 @@
-import copy
 import numpy as np
-from Connect_four_env import ConnectFour
 
-class NodeSingle():
-    def __init__(self, parent=None, env=ConnectFour(), action=None):
+class NodeSingle:
+    def __init__(self, env, state, parent=None, action_taken=None):
+        if not env.check_state_format(state):
+            print("ERROR: In state format Node constructor")
+        
         self.children = []
         self.parent = parent
-        self.action = action
+        self.action_taken = action_taken
         self.env = env
         self.reward = 0
         self.visits = 0
+        self.state = state
         
-        self.c = 4 # Exploration parameter
+        self.c = 4  # Exploration parameter
         
-    def calculate_UCB(self, child):
-        if child.visits == 0:
-            return float('inf')
+        self.expandable_moves = env.get_legal_moves_bool_array(state)
         
-        return self.env.get_player() * child.reward / child.visits + np.sqrt(self.c * np.log(self.visits) / child.visits)
-
+    def is_fully_expanded(self):
+        return np.sum(self.expandable_moves) == 0 and len(self.children) > 0
+    
     def select(self):
         node = self
-        while bool(node.children): 
-            UCB_values = []
+        best_child = self
+        while node.is_fully_expanded():
+            max_UCB = float('-inf')
             for child in node.children:
-                UCB_values.append(node.calculate_UCB(child))
-            node = node.children[np.argmax(UCB_values)]
-        
-        return node
+                UCB_value = node.get_ucb(child)
+                if UCB_value > max_UCB:
+                    max_UCB = UCB_value
+                    best_child = child
+            node = best_child
+        return best_child
+    
+    def get_ucb(self, child): # TODO: rewrite
+        if child.visits == 0:
+            return float('inf')
 
-    # Adds a child node for untried action and returns it
+        return -child.reward / child.visits + np.sqrt(self.c * np.log(self.visits) / child.visits) # TODO: Why is this negative?
+        
     def expand(self):
         if self.visits > 0:
-            legal_moves = self.env.get_legal_moves()
+            legal_moves = self.env.get_legal_moves(self.state)
             for action in legal_moves:
-                next_env = self.env.deepcopy()
-                next_env.step(action)
-                child = NodeSingle(parent=self, env=next_env, action=action)
+                self.expandable_moves[action] = 0
+                child_state = self.state.copy()
+                child_state, reward, done = self.env.step(child_state, action, 1)
+                child_state = child_state = self.env.change_perspective(child_state, player=-1)
                 
+                child = Node(self.env, child_state, self, action)
                 self.children.append(child)
-            
-    def random_act(self, env):
-        return np.random.choice(env.get_legal_moves())
-
-    # Simulate random actions until a terminal state is reached
+    
+    def random_action(self, env, state):
+        return np.random.choice(env.get_legal_moves(state))
+    
     def simulate(self):
-        simulated_game = self.env.deepcopy()
-        current_player = simulated_game.get_player() # -1 or 1
-        reward, done = simulated_game.check_game_over(current_player)
+        reward, done = self.env.check_game_over(self.state, self.action_taken)
+        reward = self.env.get_opponent_value(reward)
+        
+        if done:
+            return reward
+        
+        rollout_state = self.state.copy()
+        rollout_player = 1
         
         while not done:
-            action = self.random_act(simulated_game)
-            reward, done = simulated_game.step(action)
-
-        winning_player = simulated_game.get_player()
-        return reward * winning_player, simulated_game.board # Return 1 if the player won, -1 if the player lost, and 0 if it was a draw.
-    
-    def backpropagate(self, result):
+            rollout_action = self.random_action(self.env, rollout_state)
+            rollout_state, reward, done = self.env.step(rollout_state, rollout_action, rollout_player)
+            rollout_player = self.env.get_opponent(rollout_player)
+            
+        if self.env.get_opponent(rollout_player) == -1:
+            reward = self.env.get_opponent_value(reward)
+        return reward, rollout_state 
+            
+    def backpropagate(self, reward):
+        self.reward += reward
         self.visits += 1
-        if self.env.is_inverted:
-            self.reward -= result
-        self.reward += result
-
+        
         if self.parent:
-            self.parent.backpropagate(result)
+            opponent_reward = self.env.get_opponent_value(reward)
+            self.parent.backpropagate(opponent_reward)
+
+        
