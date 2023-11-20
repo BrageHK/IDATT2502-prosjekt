dea9b6f34ef16287499a682478781ba5b839541e
Hans Magne Asheim <hmasheim@stud.ntnu.no>
2023-11-20 11:47:22 +0100

    feature: add metod to plot game length

--- src/printLossValues.py
+++ src/printLossValues.py
@@ -2,10 +2,43 @@ import pickle
 from collections import deque
 import matplotlib.pyplot as plt
 import numpy as np
+import os
+
+def game_length(folder, batch_size):
+    with open("data/"+folder+"/game_lengths/game_lengths.pt", "rb") as file:
+        game_length = pickle.load(file)
+        
+        y_value = np.array(game_length)
+        new_y_values = []
+        
+        counter = 0
+        sum = 0
+        for i in range(0, len(y_value)):
+            if counter == batch_size:
+                new_y_values.append(sum/batch_size)
+                sum = 0
+                counter = 0
+            else:
+                sum += y_value[i]
+            counter = counter + 1
+        
+        
+        plt.axhline(y = 42, color = 'r', linestyle = 'dashed', label="42 moves") 
+        plt.plot(np.arange(1, len(new_y_values)+1), np.array(new_y_values), label="Game length")
+        plt.xlabel("Iterations")
+        plt.ylabel("Number of moves")
+        plt.title("Game length")
+        plt.legend()
+        
+        # Create the plots directory if it doesn't exist
+        if not os.path.exists('plots'):
+            os.makedirs('plots')
+        plt.savefig(f'plots/game_length.svg', format='svg')
+        plt.show()
 
 def value_loss(folder):
-    print("data/"+folder+"/loss_values-10.pk1")
-    with open("data/"+folder+"/loss_values-10.pk1", "rb") as file:
+    print("data/"+folder+"/loss_values/loss_values.pt")
+    with open("data/"+folder+"/loss_values/loss_values.pt", "rb") as file:
         print("Inside")
         value_loss = pickle.load(file)
         
@@ -17,11 +50,16 @@ def value_loss(folder):
         plt.ylabel("Loss")
         plt.title("Loss values")
         plt.legend()
+        
+        # Create the plots directory if it doesn't exist
+        if not os.path.exists('plots'):
+            os.makedirs('plots')
+        plt.savefig(f'plots/value_loss.svg', format='svg')
         plt.show()
 
 def loss(folder):
-    print("data/"+folder+"/loss_values-10.pk1")
-    with open("data/"+folder+"/loss_values-10.pk1", "rb") as file:
+    print("data/"+folder+"/loss_values/loss_values.pt")
+    with open("data/"+folder+"/loss_values/loss_values.pt", "rb") as file:
         print("Inside")
         policy_loss, value_loss = pickle.load(file)
         
@@ -45,6 +83,13 @@ def loss(folder):
         plt.ylabel("Loss")
         plt.title("Loss values")
         plt.legend()
+        
+        # Create the plots directory if it doesn't exist
+        if not os.path.exists('plots'):
+            os.makedirs('plots')
+
+        # Save the plot as SVG
+        plt.savefig(f'plots/loss.svg', format='svg')
         plt.show()
         
 def games(folder):
@@ -62,5 +107,5 @@ def create_dummy_data():
 
 folder = "ConnectFour"
 #games(folder)
-value_loss(folder)
+game_length(folder, 500)
 
